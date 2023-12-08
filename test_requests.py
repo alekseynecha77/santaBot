2413e4c578e5cb78462451ff305dc639accadc87
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2023-12-08 15:18:29 -0500

    request test

--- test_requests.py
+++ test_requests.py
@@ -2,6 +2,12 @@ import requests
 
 url = 'http://127.0.0.1:5000/ask-santa'
 data = {"query": "tell me 3 top hottest planets."}
-response = requests.post(url, json=data)
 
-print(response.text)
+try:
+    response = requests.post(url, json=data)
+    response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code
+    print(response.text)
+except requests.exceptions.HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')  # Python 3.6+
+except Exception as err:
+    print(f'An error occurred: {err}')
