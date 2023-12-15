24fb6f28c5dd87b4d4f34a76d6ab6d31c89089ef
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2023-12-14 19:08:27 -0500

    adding POST for button and GET to everything else

--- demo.py
+++ demo.py
@@ -1,5 +1,3 @@
-
-
 from flask import Flask, request, jsonify
 import boto3
 import json
@@ -10,7 +8,7 @@ app = Flask(__name__)
 def home():
     return app.send_static_file('index.html')
 
-def invoke_bedrock_model(user_input):
+def invoke_bedrock_model(prompt):
     bedrock = boto3.client(service_name='bedrock-runtime', region_name='us-east-1')
     
     input_payload = {
@@ -18,7 +16,7 @@ def invoke_bedrock_model(user_input):
         "contentType": "application/json",
         "accept": "*/*",
         "body": json.dumps({
-            "prompt": user_input,
+            "prompt": prompt,
             "max_tokens": 400,
             "temperature": 0.75,
             "p": 0.01,
@@ -32,15 +30,14 @@ def invoke_bedrock_model(user_input):
     response_body = json.loads(response['body'].read())
     return response_body
 
-@app.route('/', methods=['POST'])
-def ask_santa():
+@app.route('/santa_query', methods=['POST'])
+def santa_query():
     if not request.is_json:
         return jsonify({"error": "Missing JSON in request"}), 400
 
     data = request.get_json()
-    user_input = data.get('query', '').lower()  # Convert to lower case
+    user_input = data.get('query', '').lower()
 
-    # Santa's context for LLM
     santa_context = (
         "You are Santa Claus, embodying the spirit of Christmas. "
         "You're cheerful, jolly, and you love spreading Christmas cheer through songs and stories. "
@@ -48,17 +45,10 @@ def ask_santa():
         "Here's a new message from someone: "
     )
 
-    # Check if the user is asking Santa to sing
-    if 'sing' in user_input or 'carol' in user_input:
-        carol_prompt = "As Santa, sing a line from a Christmas carol. The message is: " + user_input
-        full_prompt = santa_context + carol_prompt
-    else:
-        full_prompt = santa_context + user_input
+    carol_prompt = f"As Santa, sing a line from a Christmas carol. The message is: {user_input}" if 'sing' in user_input or 'carol' in user_input else user_input
+    full_prompt = santa_context + carol_prompt
 
-    # Invoke the Bedrock model with the full prompt
     response_body = invoke_bedrock_model(full_prompt)
-    
-    # Return the response from the model
     return jsonify(response_body)
 
 
--- test_requests.py
+++ test_requests.py
@@ -1,13 +1,19 @@
 import requests
 
+# Updated URL to target the specific endpoint for Santa queries
 url = 'http://127.0.0.1:5000/'
+
+# Data to be sent in the request
 data = {"query": "tell me 3 top hottest planets."}
 
 try:
+    # Sending a POST request to the server
     response = requests.post(url, json=data)
-    response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code
+    response.raise_for_status()  # Raises an HTTPError for unsuccessful status codes
+
+    # Print the response from the server
     print(response.text)
 except requests.exceptions.HTTPError as http_err:
-    print(f'HTTP error occurred: {http_err}')  # Python 3.6+
+    print(f'HTTP error occurred: {http_err}')  # Handle HTTP errors
 except Exception as err:
-    print(f'An error occurred: {err}')
+    print(f'An error occurred: {err}')  # Handle other exceptions
